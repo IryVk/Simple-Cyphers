12d1262e65ca1253163050b5f766c05200a624fa
Arwa Essam Mesameh Abdelaziz <aa2101585@tkh.edu.eg>
2022-10-29 17:36:46 +0200

    fully implemented autokey cipher
    some unit tests remain

--- autokey.py
+++ autokey.py
@@ -1,11 +1,68 @@
 def main():
+#asks user for required function until valid response is given
+    while True:
+        function = input("Do you want to encrypt or decrypt? ").strip().lower()
+        if function in ["encrypt", "decrypt"]:
+            break
+        print("Invalid function, please choose one of the following (encrypt/decrypt).")
+
+    print(autokey(function))
 
     return
 
 
 def autokey(function):
+    phrase = input("Enter Text: ")
+    while True:
+        try:
+            key = input("Key: ")
+            for i in range(len(key)):
+                if ord(key[i]) < 32 or ord(key[i]) > 126:
+                    raise ValueError
+            break
+        except ValueError:
+            print("Invalid Key") #key can only contain chars within 32-126 in ascii code
+    
+    if function == "encrypt":
+        return encrypt(phrase, key)
 
-    return
+    elif function == "decrypt":
+        return decrypt(phrase, key)
+
+
+def encrypt(plaintext, key):
+    #concatenate rest of string with given key to make the key
+    for char in plaintext:
+        if ord(char) in range(32, 127):
+            key += char
+    encrypted = ""
+    counter = 0   #counter to help us repeat key until all text is encrypted
+    for i in range(len(plaintext)):
+        x = ord(plaintext[i])
+        #make sure x is in values we can change before we change it
+        if 126 >= x >= 32:
+            x = ((x + ord(key[counter]) - 32) % 95) + 32
+            counter += 1
+        encrypted += chr(x)
+        
+    
+    return encrypted
+
+
+def decrypt(ciphertext, key):
+    decrypted = ""
+    counter = 0   #counter to help us repeat key until all text is encrypted
+    for i in range(len(ciphertext)):
+        x = ord(ciphertext[i])
+        #make sure x is in values we can change before we change it
+        if 126 >= x >= 32:
+            x = ((x - ord(key[counter]) - 32 + 95) % 95) + 32
+            key += chr(x)
+            counter += 1
+        decrypted += chr(x)
+        
+    
+    return decrypted
 
 
 if __name__ == "__main__":
