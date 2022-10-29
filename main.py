29b2649d7e8bd8318ec85ed54848e20e990f72e8
Arwa Essam Mesameh Abdelaziz <aa2101585@tkh.edu.eg>
2022-10-29 23:14:12 +0200

    did some style changes

--- main.py
+++ main.py
@@ -7,29 +7,35 @@ import playfair as p
 
 
 def main():
-    #checks if command line arguments provided are valid
+    # checks if command line arguments provided are valid
     if len(sys.argv) != 3:
-        print("Invalid command line arguments.\nUsage: python main.py (encrypt/decrypt) (type of cipher)")
-        return 1
-        
-    function = sys.argv[1].lower().strip()
-    cipher = sys.argv[2].lower().strip()
-    
-    if function not in ["encrypt", "decrypt"]:
-        print("Invalid function, please choose one of the following (encrypt/decrypt).\nUsage: python main.py (encrypt/decrypt) (type of cipher)")
-        return 1
-        
-    if cipher not in ["caesar", "vigenere", "railfence", "autokey", "playfair"]:
-        print("Invalid Cipher, please choose one of the following (caesar, vigenere, railfence, autokey, playfair).\nUsage: python main.py (encrypt/decrypt) (type of cipher)")
+        print(
+            "Invalid command line arguments.\nUsage: python main.py (encrypt/decrypt) (type of cipher)"
+        )
         return 1
 
-    #runs appropriate cipher file according to chosen cipher
+    function = sys.argv[1].lower().strip()
+    cipher = sys.argv[2].lower().strip()
+
+    if function not in ["encrypt", "decrypt"]:
+        print(
+            "Invalid function, please choose one of the following (encrypt/decrypt).\nUsage: python main.py (encrypt/decrypt) (type of cipher)"
+        )
+        return 1
+
+    if cipher not in ["caesar", "vigenere", "railfence", "autokey", "playfair"]:
+        print(
+            "Invalid Cipher, please choose one of the following (caesar, vigenere, railfence, autokey, playfair).\nUsage: python main.py (encrypt/decrypt) (type of cipher)"
+        )
+        return 1
+
+    # runs appropriate cipher file according to chosen cipher
     if cipher == "caesar":
         print(c.caesar(function))
 
     elif cipher == "vigenere":
         print(v.vigenere(function))
-    
+
     elif cipher == "railfence":
         print(r.railfence(function))
 
@@ -42,6 +48,3 @@ def main():
 
 if __name__ == "__main__":
     main()
-
-    
-
