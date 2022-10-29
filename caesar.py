17d695d4fc6669cd0c413b899591ea97726236f3
Arwa Essam Mesameh Abdelaziz <aa2101585@tkh.edu.eg>
2022-10-29 15:05:29 +0200

    fully implemented caesar
    some unit tests remaining

--- caesar.py
+++ caesar.py
@@ -1,11 +1,53 @@
 def main():
+    #asks user for required function until valid response is given
+    while True:
+        function = input("Do you want to encrypt or decrypt? ").strip().lower()
+        if function in ["encrypt", "decrypt"]:
+            break
+        print("Invalid function, please choose one of the following (encrypt/decrypt).")
 
-    return
+    print(caesar(function))
 
 
 def caesar(function):
+    phrase = input("Enter Text: ")
+    while True:
+        try:
+            key = int(input("Key: "))
+            if key > 95 or key < 1:
+                raise ValueError
+            break
+        except ValueError:
+            print("Invalid Key, please enter an integer between 1 & 95") #key can be up to 95 because it's still within constraints of ascii characters we can use
 
-    return
+    if function == "encrypt":
+        return encrypt(phrase, key)
+
+    elif function == "decrypt":
+        return decrypt(phrase, key)
+
+
+def encrypt(plaintext, key):
+    encrypted = ""
+    for char in range(len(plaintext)):
+        x = ord(plaintext[char])
+        #make sure x is in values we can change before we change it
+        if 126 >= x >= 32:
+            x = ((x + key - 32) % 95) + 32
+        encrypted += chr(x)
+    
+    return encrypted
+
+
+def decrypt(plaintext, key):
+    decrypted = ""
+    for char in range(len(plaintext)):
+        x = ord(plaintext[char])
+        if 126 >= x >= 32:
+            x = ((x - key - 32) % 95) + 32
+        decrypted += chr(x)
+    
+    return decrypted
 
 
 if __name__ == "__main__":
